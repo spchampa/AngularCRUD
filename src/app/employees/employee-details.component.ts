3e39a3e3e7ffc72fe798b89da1151c603e1cc254
spchampa <st.champa@gmail.com>
2019-12-30 14:21:35 -0700

    51: Query Params

--- src/app/employees/employee-details.component.ts
+++ src/app/employees/employee-details.component.ts
@@ -30,6 +30,8 @@ export class EmployeeDetailsComponent implements OnInit {
     } else {
       this._id = 1;
     }
-    this._router.navigate(['/employees', this._id]);
+    this._router.navigate(['/employees', this._id], {
+      queryParamsHandling: 'preserve'
+    });
   }
 }
