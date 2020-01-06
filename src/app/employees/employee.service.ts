47f6cfdfef15a59640edb0fadaea0797465de94c
spchampa <st.champa@gmail.com>
2020-01-05 17:40:24 -0700

    60: Delete Form

--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -8,64 +8,69 @@ import 'rxjs/add/operator/delay';
 
 @Injectable()
 export class EmployeeService {
-    private listEmployees: Employee[] = [
-        {
-          id: 1,
-          name: 'Mark',
-          gender: 'Male',
-          contactPreference: 'Email',
-          email: 'mark@pragimtech.com',
-          dateOfBirth: new Date('10/25/1988'),
-          department: '3',
-          isActive: true,
-          photoPath: 'assets/images/mark.png'
-        },
-        {
-          id: 2,
-          name: 'Mary',
-          gender: 'Female',
-          contactPreference: 'Phone',
-          phoneNumber: 2345978640,
-          dateOfBirth: new Date('11/20/1979'),
-          department: '2',
-          isActive: true,
-          photoPath: 'assets/images/mary.png'
-        },
-        {
-          id: 3,
-          name: 'John',
-          gender: 'Male',
-          contactPreference: 'Phone',
-          phoneNumber: 5432978640,
-          dateOfBirth: new Date('3/25/1976'),
-          department: '3',
-          isActive: false,
-          photoPath: 'assets/images/john.png'
-        },
-      ];
+  private listEmployees: Employee[] = [
+    {
+      id: 1,
+      name: 'Mark',
+      gender: 'Male',
+      contactPreference: 'Email',
+      email: 'mark@pragimtech.com',
+      dateOfBirth: new Date('10/25/1988'),
+      department: '3',
+      isActive: true,
+      photoPath: 'assets/images/mark.png'
+    },
+    {
+      id: 2,
+      name: 'Mary',
+      gender: 'Female',
+      contactPreference: 'Phone',
+      phoneNumber: 2345978640,
+      dateOfBirth: new Date('11/20/1979'),
+      department: '2',
+      isActive: true,
+      photoPath: 'assets/images/mary.png'
+    },
+    {
+      id: 3,
+      name: 'John',
+      gender: 'Male',
+      contactPreference: 'Phone',
+      phoneNumber: 5432978640,
+      dateOfBirth: new Date('3/25/1976'),
+      department: '3',
+      isActive: false,
+      photoPath: 'assets/images/john.png'
+    },
+  ];
 
 
-      getEmployees(): Observable<Employee[]> {
-          return Observable.of(this.listEmployees).delay(2000); // the delay is unnecessary.
-      }
+  getEmployees(): Observable<Employee[]> {
+      return Observable.of(this.listEmployees).delay(2000); // the delay is unnecessary.
+  }
 
-      getEmployee(id: number): Employee {
-        return this.listEmployees.find(e => e.id === id);
-      }
+  getEmployee(id: number): Employee {
+    return this.listEmployees.find(e => e.id === id);
+  }
 
-      save(employee: Employee) {
-        if (employee.id === null) {
-          const maxid = this.listEmployees.reduce(function(e1,e2) {
-            return (e1.id > e2.id) ? e1 : e2;
-          }).id;
-          employee.id = maxid + 1;
-          this.listEmployees.push(employee);
-        } else {
-          const foundIndex = this.listEmployees.findIndex(e => e.id === employee.id);
-          this.listEmployees[foundIndex] = employee;
+  save(employee: Employee) {
+    if (employee.id === null) {
+      const maxid = this.listEmployees.reduce(function(e1,e2) {
+        return (e1.id > e2.id) ? e1 : e2;
+      }).id;
+      employee.id = maxid + 1;
+      this.listEmployees.push(employee);
+    } else {
+      const foundIndex = this.listEmployees.findIndex(e => e.id === employee.id);
+      this.listEmployees[foundIndex] = employee;
+    }
+  }
 
-        }
-        
-      }
+  deleteEmployee(id: number) {
+    const i = this.listEmployees.findIndex(e => e.id === id);
+    if (i !== -1) {
+      this.listEmployees.splice(i, 1);
+    }
+  }
 
 }
--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -39,6 +39,13 @@ export class ListEmployeesComponent implements OnInit {
     }
   }
 
+  onDeleteNotification(id: number) {
+    const i = this.filteredEmployees.findIndex(e => e.id === id);
+    if (i !== -1) {
+      this.filteredEmployees.splice(i, 1);
+    }
+  }
+
   ngOnInit() {
 
   }
