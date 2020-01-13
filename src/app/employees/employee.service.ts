fefc897b989277a591f39cda6b9c4363ab8f282d
spchampa <st.champa@gmail.com>
2020-01-12 21:50:43 -0700

    Part 70: httpclient delete example.

--- src/app/employees/employee.service.ts
+++ src/app/employees/employee.service.ts
@@ -14,41 +14,7 @@ import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http
 @Injectable()
 export class EmployeeService {
   constructor(private httpClient: HttpClient) {}
-  private listEmployees: Employee[] = [
-    {
-      id: 1,
-      name: 'Mark',
-      gender: 'Male',
-      contactPreference: 'Email',
-      email: 'mark@pragimtech.com',
-      dateOfBirth: new Date('10/25/1988'),
-      department: '3',
-      isActive: true,
-      photoPath: 'assets/images/mark.png'
-    },
-    {
-      id: 2,
-      name: 'Mary',
-      gender: 'Female',
-      contactPreference: 'Phone',
-      phoneNumber: 2345978640,
-      dateOfBirth: new Date('11/20/1979'),
-      department: '2',
-      isActive: true,
-      photoPath: 'assets/images/mary.png'
-    },
-    {
-      id: 3,
-      name: 'John',
-      gender: 'Male',
-      contactPreference: 'Phone',
-      phoneNumber: 5432978640,
-      dateOfBirth: new Date('3/25/1976'),
-      department: '3',
-      isActive: false,
-      photoPath: 'assets/images/john.png'
-    },
-  ];
+
 
   baseUrl = 'http://localhost:3000/employees';
 
@@ -91,11 +57,9 @@ export class EmployeeService {
     //.pipe(catchError(this.handleError));
   }
 
-  deleteEmployee(id: number) {
-    const i = this.listEmployees.findIndex(e => e.id === id);
-    if (i !== -1) {
-      this.listEmployees.splice(i, 1);
-    }
+  deleteEmployee(id: number): Observable<void> {
+    return this.httpClient.delete<void>(`${this.baseUrl}/${id}`)
+        //.pipe(catchError(this.handleError));
   }
 
 }
